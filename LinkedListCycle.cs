7046ed75cff032422c50e91a79d72f2c30aee966
Kaelyn <kaelyncampbell@Kaelyns-MacBook-Pro.local>
2021-04-02 12:39:01 -0400

    Create LinkedListCycle.cs

---
+++ LinkedListCycle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        HashSet<ListNode> map = new HashSet<ListNode>();
+
+        while (head != null)
+        {
+            if (map.Contains(head))
+            {
+                return true;
+            }
+            else
+            {
+                map.Add(head);
+                head = head.next;
+            }
+        }
+        return false;
+    }
+}
